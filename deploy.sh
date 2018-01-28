09bddb74fdac93fc8b318118dfd473dd4ca7cfdf
Sean Hignett <sean@intelligent-design.ca>
2018-01-28 09:44:06 -0700

    ci integration testing

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 GITHUB_USER=ids
 GITHUB_REPO=clairctl
+RELEASE_DESC="A CLI based utility for interacting with the Clair API"
 
 mkdir -p $GOPATH/src/$(dirname $REPO_NAME)
 ln -svf $CI_PROJECT_DIR $GOPATH/src/$REPO_NAME
@@ -16,6 +17,38 @@ go get github.com/aktau/github-release
 
 cd $CI_PROJECT_DIR
 VERSION=`cat $CI_PROJECT_DIR/VERSION`
+
+VERSION=$VERSION_$CI_COMMIT_TAG
+#git remote add github git@github.com:ids/clairctl.git
+
+echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" > ~/.git-credentials
+git config --global user.email $GITHUB_EMAIL
+git config --global user.name $GITHUB_USERNAME
+
+#if [ $(git tag -l "$VERSION") ]; then
+#  echo "${VERSION} tag already exists, deleting existing..."
+#  git tag -d $VERSION
+#  git push github :refs/tags/$VERSION  
+#  github-release delete \
+#    --user $GITHUB_USER \
+#    --repo $GITHUB_REPO \
+#    --tag $VERSION 
+#fi
+
+echo "configured remotes:"
+git remote -v
+
+echo "creating tag ${VERSION}"
+git tag -a $VERSION -m "${VERSION}"
+git push github master
+git push github --tags
+github-release release \
+  --user $GITHUB_USER \
+  --repo $GITHUB_REPO \
+  --tag $VERSION \
+  --name "${VERSION}" \
+  --description $RELEASE_DESC \
+  --pre-release  
 
 github-release upload \
     --user $GITHUB_USER \
